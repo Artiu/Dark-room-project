b2fe45123527eaab0c0218a756d1a4c6b4d17819
Artiu <oborskiartur123@gmail.com>
2021-04-23 23:04:18 +0200

    Added animation for scrolling

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener("scroll",scroll_animation);
+var scrolled = false;
+var scrolled2 = false;
+function scroll_animation()
+{
+        var info = document.getElementById("info");
+        var zarzad = document.getElementById("zarzad");
+        var bottomWindow = window.pageYOffset + window.innerHeight;
+        if(bottomWindow>=info.offsetTop && scrolled==false)
+        {
+            info.animate([
+                {opacity:0.5},
+                {opacity:1}
+            ],
+            {
+                duration:1500
+            });
+            scrolled=true;
+            console.log(scrolled);
+        }
+        if(bottomWindow>=zarzad.offsetTop && scrolled2==false)
+        {
+            zarzad.animate([
+                {opacity:0.5},
+                {opacity:1}
+            ],
+            {
+                duration:1500
+            });
+            scrolled2=true;
+            console.log(scrolled2);
+        }
+}
